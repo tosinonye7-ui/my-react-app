c64da327d25bcc52e0fa47cbdb2538ae798f80b0
tosinonye7-ui <tosinonye7@gmail.com>
2026-06-20 15:39:21 +0100

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   const isLoggedIn = true;
-  const userName = "Tosin Onyemari";
+  const userName = "Mr Charles";
   const howOld = 16;
   const sum = 2 + 3;
 
@@ -8,7 +8,7 @@ function App() {
     <h2 style={{color :"blue"}} >Welcome
       {userName} First react + vite, connecting to Github and vercel project. <br/>
       {isLoggedIn ? "Welcome Back!" : "Please Log In"}, <br />
-      You are {howOld} years old.
+      I am {howOld} years old. <br/>
       The sum of 2 and 3 is {sum}.
     </h2> 
   );
