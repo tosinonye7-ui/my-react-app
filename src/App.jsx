eba4b80e154b5565e4408edf561d8dcb6294f727
tosinonye7-ui <tosinonye7@gmail.com>
2026-06-20 15:41:50 +0100

    Fourth

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   const sum = 2 + 3;
 
   return (
-    <h2 style={{color :"blue"}} >Welcome
-      {userName} First react + vite, connecting to Github and vercel project. <br/>
+    <h2 style={{color :"blue"}} >Welcome,
+      {userName} to tosin's First react + vite, connecting to Github and vercel project. <br/>
       {isLoggedIn ? "Welcome Back!" : "Please Log In"}, <br />
       I am {howOld} years old. <br/>
       The sum of 2 and 3 is {sum}.
